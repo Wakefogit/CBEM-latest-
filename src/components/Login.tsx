da2e6796ee3cc596ed061f7f04ef2dbd39f4021d
Anandkumar B <anandkumar.b@wakeflo.com>
2023-08-23 16:20:52 +0530

    added validations

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { setAuthToken, setUser } from "../localStorage";
+import { Card, Form, Input, Button, message, Row, Col } from "antd";
 import axios from "axios";
 const Login = () => {
+  const [form] = Form.useForm();
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
   const loginApi = () => {
     let email = loginData?.email;
-
     let password = loginData?.password;
-
     console.log(email, password, "###########");
 
     // fetch("http://192.168.0.104:8080/login", {
@@ -56,9 +56,7 @@ const Login = () => {
         } else {
           alert(data.error);
         }
-      })
-
-    
+      });
   };
   return (
     <>
@@ -69,44 +67,80 @@ const Login = () => {
               <h1 className="text-xl font-bold leading-tight tracking-tight myfont text-center text-gray-900 md:text-2xl dark:text-gray">
                 Login
               </h1>
-              <form
+              <Form
                 className="space-y-4 md:space-y-6"
-                onSubmit={(e: any) => {
-                  e.preventDefault();
-                  loginApi();
-                }}
+                // onSubmit={(e: any) => {
+                //   e.preventDefault();
+                //   loginApi();
+                // }}
+                onFinish={() => loginApi()}
               >
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray myfont">
                     Email
                   </label>
-                  <input
-                    type="email"
+                  <Form.Item
                     name="email"
-                    id="email"
-                    autoComplete="off"
-                    onChange={(e) =>
-                      setLoginData({ ...loginData, email: e?.target?.value })
-                    }
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black  dark:focus:ring-blue-500 dark:focus:border-blue-500 subtitle"
-                    placeholder="Srini@Wakeflo.com"
-                  />
+                    rules={[
+                      { required: true, message: "Enter Email Id" },
+                      {
+                        validator: (_, value) => {
+                          if (value && value?.includes(" ")) {
+                            return Promise.reject("Enter valid email id");
+                          } else {
+                            return Promise.resolve();
+                          }
+                        },
+                      },
+                    ]}
+                  >
+                    <Input
+                      id="email"
+                      type="email"
+                      name="email"
+                      autoComplete="off"
+                      onChange={(e) =>
+                        setLoginData({ ...loginData, email: e?.target?.value })
+                      }
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black  dark:focus:ring-blue-500 dark:focus:border-blue-500 subtitle"
+                      placeholder="Srini@Wakeflo.com"
+                    />
+                  </Form.Item>
                 </div>
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray myfont">
                     Password
                   </label>
-                  <input
-                    type="password"
+                  <Form.Item
                     name="password"
-                    id="password"
-                    autoComplete="off"
-                    onChange={(e) =>
-                      setLoginData({ ...loginData, password: e?.target?.value })
-                    }
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 subtitle"
-                  />
+                    rules={[
+                      { required: true, message: "Enter Password" },
+                      {
+                        validator: (_, value) => {
+                          if (value && value?.includes(" ")) {
+                            return Promise.reject("Enter valid password");
+                          } else {
+                            return Promise.resolve();
+                          }
+                        },
+                      },
+                    ]}
+                  >
+                    <Input
+                      type="password"
+                      name="password"
+                      id="password"
+                      autoComplete="off"
+                      onChange={(e) =>
+                        setLoginData({
+                          ...loginData,
+                          password: e?.target?.value,
+                        })
+                      }
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 subtitle"
+                    />
+                  </Form.Item>
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
@@ -140,7 +174,7 @@ const Login = () => {
                 <p className="text-gray-500 dark:text-gray-400 text-right font-si ">
                   Powered by <span className="dark:text-gray-500">WAKEFLO</span>
                 </p>
-              </form>
+              </Form>
             </div>
           </div>
         </div>
