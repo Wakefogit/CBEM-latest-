b2a56b478e5720d1dbe3ca83b33d87323d997212
srinivas <info@wakeflo.com>
2023-08-23 12:08:40 +0530

    firxt commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import { setAuthToken, setUser } from "../localStorage";
+import axios from "axios";
+const Login = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const loginApi = () => {
+    let email = loginData?.email;
+
+    let password = loginData?.password;
+
+    console.log(email, password, "###########");
+
+    // fetch("http://192.168.0.104:8080/login", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     email: email,
+    //     password: password,
+    //   }),
+
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+
+    const apiUrl = "http://192.168.0.104:8080/login";
+    axios
+      .post(
+        apiUrl,
+        {
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        if (data.token) {
+          // Assuming setUser and setAuthToken functions are defined
+
+          setUser(data.user);
+
+          setAuthToken(data.token);
+
+          if (data.user.roleId === 1) {
+            window.location.href = "/home";
+          } else {
+            window.location.href = "/dashboard";
+          }
+        } else {
+          alert(data.error);
+        }
+      })
+
+    
+  };
+  return (
+    <>
+      <section className="bg-gray-50 bg-[url('/src/assets/images/bg.png')] bg-cover drop-shadow-2xl ">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 drop-shadow-lg">
+          <div className="w-full bg-white rounded-sm shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gradient-to-r from-white-600 to-white-500 dark:border-gray-700 opacity-95">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight myfont text-center text-gray-900 md:text-2xl dark:text-gray">
+                Login
+              </h1>
+              <form
+                className="space-y-4 md:space-y-6"
+                onSubmit={(e: any) => {
+                  e.preventDefault();
+                  loginApi();
+                }}
+              >
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray myfont">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    onChange={(e) =>
+                      setLoginData({ ...loginData, email: e?.target?.value })
+                    }
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black  dark:focus:ring-blue-500 dark:focus:border-blue-500 subtitle"
+                    placeholder="Srini@Wakeflo.com"
+                  />
+                </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray myfont">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    onChange={(e) =>
+                      setLoginData({ ...loginData, password: e?.target?.value })
+                    }
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 subtitle"
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label className="text-gray-900 dark:text-gray-900 subtitle">
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 text-gray-600"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-purple-900 py-2 text-white rounded"
+                >
+                  Sign in
+                </button>
+                <p className="text-gray-500 dark:text-gray-400 text-right font-si subtitle">
+                  Powered by <span className="dark:text-gray-500">WAKEFLO</span>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
