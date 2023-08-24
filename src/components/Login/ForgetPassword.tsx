e6e894e81b4706cb6878d016a4ffdd8cb733d762
Anandkumar B <anandkumar.b@wakeflo.com>
2023-08-24 15:26:30 +0530

    merged with backend

--- src/components/Login/ForgetPassword.tsx
+++ src/components/Login/ForgetPassword.tsx
@@ -1,13 +1,38 @@
-import React from "react";
-import { Link,Navigate, useNavigate } from "react-router-dom";
-
-import { Card, Form, Input, Button, message, Row, Col } from "antd";
+import React, { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Card, Form, Input, Button, message} from "antd";
 const ForgetPassword = () => {
-    const navigate = useNavigate()
-    const [form] = Form.useForm();
-    const otpHandler = () =>{
-        navigate('/verify-otp')
-    }
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const [loginemail, setloginemail] = useState({
+    email: "",
+  });
+  const otpHandler = () => {
+    const apiUrl = "http://192.168.0.104:8080/forgotpassword";
+
+    axios
+      .post(
+        apiUrl,
+        {
+          email: loginemail?.email,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        if (data.message) {   
+            message.success(data.message)      
+            navigate("/verify-otp")
+        } else {
+          alert(data.error);
+        }
+      });
+  };
   return (
     <>
       <section className="bg-gray-50 bg_log bg-cover h-screen flex items-center justify-center">
@@ -16,9 +41,9 @@ const ForgetPassword = () => {
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight myfont text-gray-900 md:text-3xl dark:text-gray">
                 Forgot Password
-              </h1>            
+              </h1>
 
-              <Form className="space-y-4 md:space-y-6" action="#">
+              <Form className="space-y-4 md:space-y-6" form={form}>
                 <div className="-mb-5">
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray myfont">
                     Email
@@ -43,7 +68,7 @@ const ForgetPassword = () => {
                       type="email"
                       name="email"
                       autoComplete="off"
-                     
+                      onChange={(e) => setloginemail({ email: e.target.value })}
                       className="bg-gray-50 border  border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black  dark:focus:ring-blue-500 dark:focus:border-blue-500 subtitle"
                       placeholder="Srini@Wakeflo.com"
                     />
